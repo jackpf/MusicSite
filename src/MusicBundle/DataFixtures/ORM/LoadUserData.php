163731ea9742f79066d2c36e255c187ccae8b0b5
Jack Farrelly <jack_farrelly@yahoo.co.uk>
2015-05-25 00:39:57 +0100

    User stuff

---
+++ src/MusicBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MusicBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use MusicBundle\Entity\User;
+
+class LoadUserData implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setEmail('admin@smileyaudio.com');
+        $user->setPlainPassword('pixie');
+        $user->addRole('ROLE_USER');
+        $user->addRole('ROLE_ADMIN');
+        $user->setExpired(false);
+        $user->setEnabled(true);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}

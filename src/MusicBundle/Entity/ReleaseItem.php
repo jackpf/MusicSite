bbc0dc37d75769968cb96b1c2267598a1fa0e66f
Jack Farrelly <jack_farrelly@yahoo.co.uk>
2015-05-25 05:23:05 +0100

    Layout

--- src/MusicBundle/Entity/ReleaseItem.php
+++ src/MusicBundle/Entity/ReleaseItem.php
@@ -4,5 +4,15 @@ namespace MusicBundle\Entity;
 
 class ReleaseItem extends MediaItem
 {
-    
+    private $path;
+
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    public function setPath($path)
+    {
+        $this->path = $path;
+    }
 }

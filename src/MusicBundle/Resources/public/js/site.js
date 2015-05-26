11f14069942a36841973e91c2e3aaad42fa36d84
Jack Farrelly <jack_farrelly@yahoo.co.uk>
2015-05-26 07:47:34 +0100

    Focus search when opened

--- src/MusicBundle/Resources/public/js/site.js
+++ src/MusicBundle/Resources/public/js/site.js
@@ -2,6 +2,7 @@ $('#search-link').click(function() {
     $box = $('#search-box');
     if (!$box.is(':visible')) {
         $box.slideDown();
+        $box.children().first().focus();
         $(this).addClass('active');
     } else {
         $box.slideUp();

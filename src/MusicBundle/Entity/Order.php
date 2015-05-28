16a2791821c7db7b2c7dd3c499739c6e1fe71311
Jack Farrelly <jack_farrelly@yahoo.co.uk>
2015-05-28 04:00:06 +0100

    Order entity

---
+++ src/MusicBundle/Entity/Order.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace MusicBundle\Entity;
+
+class Order
+{
+    private $id;
+
+    private $user;
+
+    private $mediaVariant;
+
+    private $price;
+
+    private $status;
+
+    private $createdAt;
+
+    private $updatedAt;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    public function getMediaVariant()
+    {
+        return $this->mediaVariant;
+    }
+
+    public function setMediaVariant($mediaVariant)
+    {
+        $this->mediaVariant = $mediaVariant;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+}

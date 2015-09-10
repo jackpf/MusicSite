d131ac7d08721b3cab1817721460c7f4e3f1dd64
Jack Farrelly <jack_farrelly@yahoo.co.uk>
2015-09-10 22:57:21 +0100

    Order pagination and menu

--- src/Application/Sonata/UserBundle/Controller/OrderController.php
+++ src/Application/Sonata/UserBundle/Controller/OrderController.php
@@ -3,15 +3,20 @@
 namespace Application\Sonata\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class OrderController extends Controller
 {
-    public function ordersAction()
+    public function ordersAction(Request $request)
     {
         $user = $this->get('security.context')->getToken()->getUser();
+
         $orders = $this->getDoctrine()->getEntityManager()
             ->getRepository('MusicBundle\Entity\Order')
             ->findByUser($user, ['createdAt' => 'desc']);
+
+        $orders = $this->get('knp_paginator')
+            ->paginate($orders, $request->get('page', 1), 10);
 
         return $this->render('FOSUserBundle:Profile:orders.html.twig',[
             'orders' => $orders,
--- src/MusicBundle/Resources/public/js/site.js
+++ src/MusicBundle/Resources/public/js/site.js
@@ -37,5 +37,16 @@ $('#search-link').click(function(e) {
 // Other stuff
 $(document).ready(function() {
     $(".fancybox").fancybox();
+
     $(".js-tooltip").tooltipster();
+
+    $('nav li ul').hide().removeClass('fallback');
+    $('nav li').hover(
+        function () {
+            $('ul', this).stop().slideDown(200);
+        },
+        function () {
+            $('ul', this).stop().slideUp(200);
+        }
+    );
 });

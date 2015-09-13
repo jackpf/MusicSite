853162d03673df14f1fb9cb0ea7ba71d2335d87e
Jack Farrelly <jack_farrelly@yahoo.co.uk>
2015-09-13 21:14:02 +0100

    News category controllers

--- src/MusicBundle/Controller/MainController.php
+++ src/MusicBundle/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace MusicBundle\Controller;
 
+use MusicBundle\Data\Data;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -16,23 +17,44 @@ class MainController extends Controller
         return $this->render('MusicBundle:Music:_news_categories.html.twig', ['categories' => $categories]);
     }
 
-    public function indexAction(Request $request, $type = null)
+    public function indexAction(Request $request, $repo = null)
     {
         $em = $this->getDoctrine()->getEntityManager();
-
-        if ($type == 'news') {
-            $repo = 'NewsItem';
-        } else if ($type == 'release') {
-            $repo = 'ReleaseItem';
-        } else if ($type == 'mix') {
-            $repo = 'MixItem';
-        } else {
-            $repo = 'MediaItem';
-        }
 
         $qb = $em->getRepository(sprintf('MusicBundle:%s', $repo))
             ->createQueryBuilder('i')
             ->select('i')
+            ->orderBy('i.createdAt', 'desc');
+
+        if ($request->query->has('search')) {
+            $s = $qb->expr()->literal('%' . str_replace(' ', '%', $request->query->get('search')) . '%');
+            $qb->andWhere($qb->expr()->like('i.title', $s));
+        }
+
+        $items = $this->get('knp_paginator')
+            ->paginate($qb->getQuery(), $request->get('page', 1), 9);
+
+        return $this->render('MusicBundle:Music:index.html.twig', [
+            'items' => $items
+        ]);
+    }
+
+    public function newsCategoryAction(Request $request, $slug)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $category = $em->getRepository('MusicBundle:NewsCategory')
+            ->findBySlug($slug);
+
+        if (!$category) {
+            throw $this->createNotFoundException('Category not found');
+        }
+
+        $qb = $em->getRepository('MusicBundle:NewsItem')
+            ->createQueryBuilder('i')
+            ->select('i')
+            ->where(':category member of i.categories')
+            ->setParameter('category', $category)
             ->orderBy('i.createdAt', 'desc');
 
         if ($request->query->has('search')) {

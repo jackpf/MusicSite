2dc9e99a0ebd2b64b71ddcb8debbf9f53271c8d4
Jack Farrelly <jack_farrelly@yahoo.co.uk>
2015-09-17 06:58:50 +0100

    Fix error redirection for non http exceptions

--- src/MusicBundle/EventListener/KernelListener.php
+++ src/MusicBundle/EventListener/KernelListener.php
@@ -5,6 +5,7 @@ namespace MusicBundle\EventListener;
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Kernel;
 
 class KernelListener
@@ -25,7 +26,7 @@ class KernelListener
             $exception = $event->getException();
 
             $event->setResponse(
-                new RedirectResponse($this->router->generate('music_error', ['code' => $exception->getStatusCode()]))
+                new RedirectResponse($this->router->generate('music_error', ['code' => $event->getException() instanceof HttpException ? $exception->getStatusCode() : 500]))
             );
         }
     }

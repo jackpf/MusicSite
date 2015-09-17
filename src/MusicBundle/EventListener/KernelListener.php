b9a376d9c4f4630e2ac6106eb7eb2e1da37487e5
Jack Farrelly <jack_farrelly@yahoo.co.uk>
2015-09-17 05:13:42 +0100

    Error page

---
+++ src/MusicBundle/EventListener/KernelListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MusicBundle\EventListener;
+
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Kernel;
+
+class KernelListener
+{
+    private $kernel;
+
+    private $router;
+
+    public function __construct(Kernel $kernel, Router $router)
+    {
+        $this->kernel = $kernel;
+        $this->router = $router;
+    }
+
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        if ($this->kernel->getEnvironment() != 'dev') {
+            $exception = $event->getException();
+
+            $event->setResponse(
+                new RedirectResponse($this->router->generate('music_error', ['code' => $exception->getStatusCode()]))
+            );
+        }
+    }
+}

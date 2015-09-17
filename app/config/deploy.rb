3e2765e44d2fc1a40fd852da498a7dc2fbce92b8
Jack Farrelly <jack_farrelly@yahoo.co.uk>
2015-09-17 05:13:07 +0100

    New deployment

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, "Regulation17"
-set :domain,      "regulation17.noip.me"
-set :user,        "osmc"
-set :deploy_to,   "/home/#{user}/workspace/#{domain}"
+set :domain,      "185.116.212.11"
+set :user,        "jack"
+set :deploy_to,   "/home/#{user}/websites/#{domain}"
 set :app_path,    "app"
 
 set :repository,  "https://github.com/jackpf/Regulation17.git"
@@ -14,7 +14,7 @@ set :model_manager, "doctrine"
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
-set   :use_sudo,      false
+set  :use_sudo,      false
 set  :keep_releases,  3
 
 # Be more verbose by uncommenting the following line
@@ -24,7 +24,7 @@ set :shared_files,      ["app/config/parameters.yml"]
 set :dump_assetic_assets, true
 
 after 'deploy:create_symlink' do
-    run "cd #{deploy_to}/current && sudo setfacl -R -m u:www-data:rwX -m u:`whoami`:rwX app/cache app/logs && sudo setfacl -dR -m u:www-data:rwX -m u:`whoami`:rwX app/cache app/logs"
+    run "cd #{deploy_to}/current && setfacl -R -m u:www-data:rwX -m u:`whoami`:rwX app/cache app/logs && setfacl -dR -m u:www-data:rwX -m u:`whoami`:rwX app/cache app/logs"
     run "cd #{deploy_to}/current && php app/console doctrine:schema:update --force --env=prod"
 end
 

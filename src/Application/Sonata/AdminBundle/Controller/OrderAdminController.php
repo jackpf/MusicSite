6350dc31f9627cd0713935026f15b8607722523c
Jack Farrelly <jack_farrelly@yahoo.co.uk>
2015-09-18 22:16:45 +0100

    Order dispatched email notification

--- src/Application/Sonata/AdminBundle/Controller/OrderAdminController.php
+++ src/Application/Sonata/AdminBundle/Controller/OrderAdminController.php
@@ -3,8 +3,10 @@
 namespace Application\Sonata\AdminBundle\Controller;
 
 use MusicBundle\Entity\Order;
+use Payum\Core\Request\GetHumanStatus;
 use Sonata\AdminBundle\Controller\CRUDController;
 use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
@@ -18,14 +20,27 @@ class OrderAdminController extends CRUDController
 
         $modelManager = $this->admin->getModelManager();
         $selectedModels = $selectedModelQuery->execute();
-
+        $eventDispatcher = $this->get('event_dispatcher');
         $i = 0;
 
         try {
             foreach ($selectedModels as $selectedModel) {
+                if ($selectedModel->getDispatchStatus() == Order::DISPATCH_STATUS_DISPATCHED) {
+                    $this->addFlash('sonata_flash_error', sprintf('Order %d has already been dispatched.', $selectedModel->getId()));
+                    continue;
+                }
+                if ($selectedModel->getDispatchStatus() == Order::DISPATCH_STATUS_UNDISPATCHABLE) {
+                    $this->addFlash('sonata_flash_error', sprintf('Order %d is not dispatchable.', $selectedModel->getId()));
+                    continue;
+                }
+                if ($selectedModel->getStatus() != GetHumanStatus::STATUS_AUTHORIZED) {
+                    $this->addFlash('sonata_flash_error', sprintf('Order %d has not been authorized.', $selectedModel->getId()));
+                    continue;
+                }
+
                 if ($selectedModel->getDispatchStatus() == Order::DISPATCH_STATUS_PROCESSING) {
                     $selectedModel->setDispatchStatus(Order::DISPATCH_STATUS_DISPATCHED);
-
+                    $eventDispatcher->dispatch('event.dispatch', new GenericEvent(null, ['order' => $selectedModel]));
                     $i++;
                 }
             }
@@ -39,7 +54,9 @@ class OrderAdminController extends CRUDController
             );
         }
 
-        $this->addFlash('sonata_flash_success', sprintf('Marked %d orders as dispatched', $i));
+        if ($i > 0) {
+            $this->addFlash('sonata_flash_success', sprintf('Marked %d orders as dispatched.', $i));
+        }
 
         return new RedirectResponse(
             $this->admin->generateUrl('list', $this->admin->getFilterParameters())
--- src/MusicBundle/Email/EmailListener.php
+++ src/MusicBundle/Email/EmailListener.php
@@ -26,4 +26,11 @@ class EmailListener
         $this->mailer->send($order->getUser()->getEmail(), 'MusicBundle:Email:order.html.twig', ['order' => $order]);
         $this->mailer->send($this->mailerUser, 'MusicBundle:Email:order_admin.html.twig', ['order' => $order]);
     }
+
+    public function dispatchEvent(GenericEvent $event)
+    {
+        $order = $event->getArgument('order');
+
+        $this->mailer->send($order->getUser()->getEmail(), 'MusicBundle:Email:dispatch.html.twig', ['order' => $order]);
+    }
 }

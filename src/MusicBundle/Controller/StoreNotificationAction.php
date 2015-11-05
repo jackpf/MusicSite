6c6823e28ee55cb61730218117b2ebd182604df7
Jack Farrelly <jack_farrelly@yahoo.co.uk>
2015-11-05 16:32:08 +0000

    Paypal notification

--- src/MusicBundle/Controller/StoreNotificationAction.php
+++ src/MusicBundle/Controller/StoreNotificationAction.php
@@ -2,18 +2,68 @@
 
 namespace MusicBundle\Controller;
 
+use MusicBundle\Entity\Order;
 use Payum\Core\Action\GatewayAwareAction;
 use Payum\Core\Request\Notify;
-use Payum\Core\Storage\StorageInterface;
 use Payum\Core\Request\GetHttpRequest;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Payum\Core\Request\GetHumanStatus;
 
 class StoreNotificationAction extends GatewayAwareAction
 {
-    protected $notificationStorage;
+    protected $container;
 
-    public function __constructor(StorageInterface $notificationStorage)
+    public function setContainer(ContainerInterface $container)
     {
-        $this->notificationStorage = $notificationStorage;
+        $this->container = $container;
+    }
+
+    private function verify($notification)
+    {
+        $postData = 'cmd=_notify-validate';
+
+        foreach ($notification as $key => $value) {
+            $postData .= '&' . $key . '=' . $value;
+        }
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $this->container->getParameter('paypal_sandbox') ? 'https://www.sandbox.paypal.com/cgi-bin/webscr' : 'https://www.paypal.com/cgi-bin/webscr');
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        return curl_exec($ch) == 'VERIFIED';
+    }
+
+    private function updateOrderStatus(Order $order)
+    {
+        switch ($order->getNotification()['payment_status']) {
+            case 'Completed':
+                $order->setStatus(GetHumanStatus::STATUS_AUTHORIZED);
+            break;
+            case 'Denied': case 'Failed': case 'Voided':
+                $order->setStatus(GetHumanStatus::STATUS_FAILED);
+            break;
+            case 'Expired':
+                $order->setStatus(GetHumanStatus::STATUS_EXPIRED);
+            break;
+            case 'Pending':
+                $order->setStatus(GetHumanStatus::STATUS_PENDING);
+            break;
+            case 'Refunded':
+                $order->setStatus(GetHumanStatus::STATUS_REFUNDED);
+            break;
+            case 'Reversed':
+                $order->setStatus('reversed' /* no existing state for this */);
+            break;
+            case 'Processed':
+                $order->setStatus(GetHumanStatus::STATUS_PENDING);
+            break;
+            default:
+                $order->setStatus(GetHumanStatus::STATUS_UNKNOWN);
+            break;
+        }
     }
 
     public function execute($request)
@@ -28,7 +78,16 @@ class StoreNotificationAction extends GatewayAwareAction
             $notification[$name] = $value;
         }
 
-        $order->setNotification($notification);
+        //$order->setNotification($notification);
+
+        if (!$this->verify($order->getNotification())) {
+            throw new \RuntimeException('PayPal did not verify this notification');
+        }
+
+        $this->updateOrderStatus($order);
+
+        $this->container->get('doctrine.orm.entity_manager')
+            ->flush();
     }
 
     public function supports($request)
